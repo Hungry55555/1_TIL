124e3b429be5259880e692777fcea7e392088375
nyh <dnyh@gmail.com>
2024-01-15 11:47:38 +0900

    second commit TIL

---
+++ git/python/string-sample.py
@@ -0,0 +1,18 @@
+# print('이 다음은 엔터\n입니다.')
+
+
+bugs = 'roaches'
+counts = 13
+area = 'living room'
+
+# Debugging roaches 13 living room
+print(f'Debugging {bugs} {counts} {area}')
+
+my_str = 'hello'
+
+# my_str[1] = 'z'
+L=list(my_str)
+L = L[1]
+print(L)
+
+print(my_str[0 :100:100])
