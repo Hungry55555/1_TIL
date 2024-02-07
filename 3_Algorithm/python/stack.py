f101147013422285ed9d6c762002c8e806516551
yh <nyh1142@gmail.com>
2024-02-07 17:49:48 +0900

    stack1

---
+++ 3_Algorithm/python/stack.py
@@ -0,0 +1,20 @@
+import sys
+
+a = []
+print(sys.getsizeof(a))
+a.append(1)
+print(sys.getsizeof(a))
+a.append(2)
+print(sys.getsizeof(a))
+a.append(3)
+print(sys.getsizeof(a))
+a.append(4)
+print(sys.getsizeof(a))
+a.append(5)
+print(sys.getsizeof(a))
+a.append(6)
+print(sys.getsizeof(a))
+a.append(7)
+print(sys.getsizeof(a))
+a.append(8)
+
